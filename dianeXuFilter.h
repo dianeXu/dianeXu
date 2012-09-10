1503329c3b772629f661a22188d253fb821dd740
Beegz <beegz@beegz.net>
2012-09-10 10:46:28 +0200

    Added Base Framework
    All parts of the basic plugin project have been added.

---
+++ dianeXuFilter.h
@@ -0,0 +1,30 @@
+//
+//  dianeXuFilter.h
+//  This file is part of dianeXu <http://www.dianeXu.com>.
+//
+//  dianeXu is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  dianeXu is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Copyright (c) 2012 Dipl.Ing.(FH) Bjšrn Schwarz <beegz@dianeXu.com>. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <OsiriXAPI/PluginFilter.h>
+
+@interface dianeXuFilter : PluginFilter {
+
+}
+
+- (long) filterImage:(NSString*) menuName;
+
+@end
