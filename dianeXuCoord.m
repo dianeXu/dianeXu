d84658f784e845031a37946b51760efb5b96d6b2
Beegz <beegz@beegz.net>
2012-11-30 10:29:51 +0100

    Coord updated
    Updated the Coord class with licens information.

--- dianeXuCoord.m
+++ dianeXuCoord.m
@@ -1,9 +1,21 @@
 //
-//  dianeXuCoord.m
-//  dianeXu
+//  dianeXuCoord.mh
+//  This file is part of dianeXu <http://www.dianeXu.com>.
 //
-//  Created by Björn Schwarz on 11/30/12.
+//  dianeXu is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //
+//  dianeXu is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with dianeXu.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Copyright (c) 2012 Dipl.Ing.(FH) Björn Schwarz <beegz@dianeXu.com>. All rights reserved.
 //
 
 #import "dianeXuCoord.h"
