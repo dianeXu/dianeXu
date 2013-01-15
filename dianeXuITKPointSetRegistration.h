c107905cb2c259643c6a3905a895436cd4646201
Beegz <beegz@beegz.net>
2013-01-15 08:08:29 +0100

    ITKPointSetRegistrationClass added
    Added an ITKPointSetRegistration class to handle the EAM->Angiomodel
    registration.

---
+++ dianeXuITKPointSetRegistration.h
@@ -0,0 +1,27 @@
+//
+//  dianeXuITKPointSetRegistration.h
+//  This file is part of dianeXu <http://www.dianeXu.com>.
+//
+//  dianeXu is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  dianeXu is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with dianeXu.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Copyright (c) 2012-2013 Dipl.Ing.(FH) Björn Schwarz <beegz@dianeXu.com>. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dianeXuITKPointSetRegistration : NSObject {
+    
+}
+
+@end
